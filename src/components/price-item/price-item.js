eb9b50322090faaeb85e0ad2a076d8eba7ac9570
Mariya Skosyrskaya <merymeggs@yandex.ru>
2021-05-01 23:18:40 +0300

    updeted styles

--- src/components/price-item/price-item.js
+++ src/components/price-item/price-item.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import cn from 'classnames';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
-import styles from './price-item.module.css';
 
 function PriceItem({ price, classMarg, classText }) {
 	return (
