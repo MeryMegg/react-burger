b17eb965b7c93d179c102931bed290524c6b3d9b
Mariya Skosyrskaya <merymeggs@yandex.ru>
2021-06-02 13:33:13 +0300

    fix: remove unnecessary imports

--- src/components/burger-ingredient/burger-ingredient.js
+++ src/components/burger-ingredient/burger-ingredient.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import cn from 'classnames';
 import { Counter } from '@ya.praktikum/react-developer-burger-ui-components';
