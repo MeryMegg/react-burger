f758b962d182d5cf3b753af162559eebbe09d8cc
Mariya Skosyrskaya <merymeggs@yandex.ru>
2021-06-07 17:01:40 +0300

    fix: update NavLink

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import cn from 'classnames';
 import { BurgerIcon, ListIcon, ProfileIcon, Logo } from '@ya.praktikum/react-developer-burger-ui-components';
-import { NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useRouteMatch } from 'react-router-dom';
 import NavBar from '../nav-bar/nav-bar';
 import styles from './app-header.module.css';
 
 function AppHeader() {
-	const { pathname } = useLocation()
+	const isConstructor = !!useRouteMatch({ path: '/', exact: true });
+	const isFeed = !!useRouteMatch('/feed');
+	const isProfile = !!useRouteMatch('/profile');
 	return (
 		<header className={cn('pl-10', 'pr-10', 'pt-4', 'pb-4', 'text', 'text_type_main-default', styles.header)}>
 			<NavBar>
 				<>
 					<div className={cn(styles.menu)}>
 						<NavLink exact to="/" className={cn(styles.link, 'pt-4', 'pb-4', 'pr-5', 'mr-2')} activeClassName={styles.link_active} >
-							<BurgerIcon type={pathname === '/' ? "primary" : "secondary"} />
+							<BurgerIcon type={isConstructor ? "primary" : "secondary"} />
 							<span className={cn('ml-2')}>Конструктор</span>
 						</NavLink>
 						<NavLink to="/feed" activeClassName={styles.link_active} className={cn(styles.link, 'pt-4', 'pb-4', 'pr-5', 'pl-5')}>
-							<ListIcon type={pathname === '/feeda' ? "primary" : "secondary"} />
+							<ListIcon type={isFeed ? "primary" : "secondary"} />
 							<span className={cn('ml-2')}>Лента заказов</span>
 						</NavLink>
 					</div>
@@ -25,7 +27,7 @@ function AppHeader() {
 						<Logo />
 					</NavLink>
 					<NavLink to="/profile" activeClassName={styles.link_active} className={cn(styles.link)}>
-						<ProfileIcon type={pathname === '/profile' ? "primary" : "secondary"} />
+						<ProfileIcon type={isProfile ? "primary" : "secondary"} />
 						<span className={cn('pt-4', 'pb-4', 'pl-5')}>Личный кабинет</span>
 					</NavLink>
 				</>
